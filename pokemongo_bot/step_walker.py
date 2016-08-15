14738d64cc3e444ab2b9b793154eeebeb42bf8d8
Jamezin <jstanley808@gmail.com>
2016-08-14 20:21:53 -1000

    Added randomize speed to walking.
    Randomizes speed of current config speed by 50%

--- pokemongo_bot/step_walker.py
+++ pokemongo_bot/step_walker.py
@@ -1,4 +1,5 @@
 from math import sqrt
+from random import random
 
 from cell_workers.utils import distance
 from human_behaviour import random_lat_long_delta, sleep
@@ -19,7 +20,7 @@ class StepWalker(object):
             dest_lng
         )
 
-        self.speed = speed
+        self.speed = speed + speed * 0.5 * ((random()*2) - 1)
 
         self.destLat = dest_lat
         self.destLng = dest_lng
